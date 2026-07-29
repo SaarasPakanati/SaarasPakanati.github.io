889bb755fc5361faca86ceb34764d226d1ff3e33
SaarasPakanati <saarasps@gmail.com>
2026-07-29 13:12:53 +0000

    Deploying to gh-pages from @ SaarasPakanati/SaarasPakanati.github.io@ac751d88f647b2e71a0d123ab01200b6e18f71d2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-research",
-          title: "research",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/research/";
-          },
-        },{id: "nav-repositories",
+  },{id: "nav-repositories",
           title: "repositories",
           description: "Open-source contributions and personal projects.",
           section: "Navigation",
